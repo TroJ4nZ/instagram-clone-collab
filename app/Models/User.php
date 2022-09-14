a6136ed5ff6ca3594ac95e937b55da3ee81378c4
TroJ4nZ <trojanz1258@gmail.com>
2022-09-15 01:23:19 +0200

    Fix repo conflict by adding following function

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,8 @@ class User extends Authenticatable
     {
         return $this->hasOne(Profile::class);
     }
+
+    public function following(){
+        return $this->belongsToMany(Profile::class);
+    }
 }
