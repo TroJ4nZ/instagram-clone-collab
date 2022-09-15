ba3c00355e7fd99289b2faa7daf6c499be73739a
TroJ4nZ <trojanz1258@gmail.com>
2022-09-15 14:05:56 +0200

    Fix merge conflict

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
-use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -59,7 +58,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        // return view('profile.edit')->with(['user' => Auth::user()]);
+        //
     }
 
     /**
