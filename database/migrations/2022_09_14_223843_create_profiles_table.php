7df3ce09eb6b06ff80485c45c99a9fbef5f59d28
TroJ4nZ <trojanz1258@gmail.com>
2022-09-15 02:07:41 +0200

    Fix id field in Profile model

--- database/migrations/2022_09_14_223843_create_profiles_table.php
+++ database/migrations/2022_09_14_223843_create_profiles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->id('profile_id');
+            $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('title')->nullable();
